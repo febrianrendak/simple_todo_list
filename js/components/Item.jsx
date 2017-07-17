d3b83574ffb162d68348eaeecd070436d2b1d0e8
febrianrendak <febriananugrah92@gmail.com>
2017-07-17 13:36:39 +0700

    beautification

--- js/components/Item.jsx
+++ js/components/Item.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 export default class Item extends React.Component {
     render() {
         return(
-            <li>
-                <span onClick={()=> { this.props.toggleMark(this.props.item.id) }} className={ this.props.item.done ? 'item-done' : '' }>{this.props.item.text}</span> <span onClick={() => { this.props.removeItem(this.props.item.id) }}>[x]</span>
+            <li className="item">
+                <span onClick={()=> { this.props.toggleMark(this.props.item.id) }} className={ this.props.item.done ? 'item-done item-text' : 'item-text' }>
+                    {this.props.item.text}
+                </span>
+                <div className="item-delete btn btn-xs btn-danger" onClick={() => { this.props.removeItem(this.props.item.id) }}>
+                    <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                </div>
             </li>
         )
     }
